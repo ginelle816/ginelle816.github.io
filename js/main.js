5d953d08d1b74a7c7ee4d6a7e04e0d8b77c92c87
ginelle816 <ginelle@yesware.com>
2017-01-18 03:22:04 -0800

    final project updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-$('#gallery img').on('hover', function () {
-    
+// $('.grid-wrapper img').on('hover', function () {
+//     $(this).html($("<span> HOVERING!!!!! </span>"));
+// });
+
+
+
+
+// $(document).ready(function() {
+//     $('.grid-wrapper img').animate({
+//         opacity:1
+
+//     });
+
+//     $('.grid-wrapper img').hover(function() {
+//         $(this).stop().animate({opacity:.4},200);
+
+//     }, function() {
+//         $(this).stop().animate({opacity:1},500)
+
+//     });
+
+// });
+
+
+ $('.overlay').hover(function () {
+    $(this).toggleClass('overlay-hover');
+ });
+
+$('.hamburger').on('click', function (event) {
+	// Slide toggle the menu (#dropdown)
+	event.preventDefault();
+    $('#dropdown').slideToggle(300);
+    // Bonus: If the user clicks anywhere outside of the menu slide the menu up
 });
