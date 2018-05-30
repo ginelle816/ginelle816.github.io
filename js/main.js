2e88191f1af908e654bc35e07435483c778eae7e
ginelle816 <ginelle@yesware.com>
2018-05-29 18:11:41 -0700

    test

--- js/main.js
+++ js/main.js
@@ -42,23 +42,23 @@ if (windowWidth > 500) {
 
 
 
-// Smooth scroll this mother!
+// // Smooth scroll this mother!
 
-// Do it when someone clicks a nav link
-$('.smooth-scroll').on('click', function(e) {
-  // prevent the standard link operation on click
-  e.preventDefault();
-  // use the href of the link to identify what
-  // section to scroll to
-  var thisTarget = $(this).attr('href');
-  // get that section's top offset
-  var targetOffset = $(thisTarget).offset().top;
-  // use jQuery.animate() to animate the body's
-  // scrollTop to the targetOffest
-  $('body').animate({
-    scrollTop: targetOffset
-  }, 600);
-});
+// // Do it when someone clicks a nav link
+// $('.smooth-scroll').on('click', function(e) {
+//   // prevent the standard link operation on click
+//   e.preventDefault();
+//   // use the href of the link to identify what
+//   // section to scroll to
+//   var thisTarget = $(this).attr('href');
+//   // get that section's top offset
+//   var targetOffset = $(thisTarget).offset().top;
+//   // use jQuery.animate() to animate the body's
+//   // scrollTop to the targetOffest
+//   $('body').animate({
+//     scrollTop: targetOffset
+//   }, 600);
+// });
 
 
 
