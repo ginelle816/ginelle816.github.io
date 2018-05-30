c6f49f8b9939e0a665132f303efa8fcb12c814c2
ginelle816 <ginelle@yesware.com>
2018-05-29 18:20:49 -0700

    new smooth scroll

--- js/main.js
+++ js/main.js
@@ -42,7 +42,32 @@ if (windowWidth > 500) {
 
 
 
-// // Smooth scroll this mother!
+// Smooth scroll this mother!
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (600) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
 
 // // Do it when someone clicks a nav link
 // $('.smooth-scroll').on('click', function(e) {
