0d2e67aeb088b8a866603a452874522dfa645f45
ginelle816 <ginelle@yesware.com>
2018-05-29 20:22:37 -0700

    form test

--- js/main.js
+++ js/main.js
@@ -127,19 +127,36 @@ $('form').on('submit', function (e) {
 
 });
 
-// Validate form
-$('form').validate({ // initialize the plugin
+// Form validation
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  $("form[name='contact']").validate({
+    // Specify validation rules
     rules: {
-        name: {
-            required: true,
-        },
-        email: {
-            required: true,
-            email: true
-        }
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      name: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      name: "Please enter your firstname",
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
     }
+  });
 });
-
 
 
 // KONAMI!
